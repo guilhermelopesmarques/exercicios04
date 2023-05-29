35fc75d03a3947c0b4bf9dce630ab6cd13dfc684
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-05-29 19:19:44 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio04</title>
+</head>
+<body>
+    <form>
+    Coloque seu Número:
+    <input type="int" name="numero">
+
+    </form>
+</body>
+</html>
